cac40ceeb7539b65010b7a6ad0a4320d35684fb3
dani <dani.zaghian@gmail.com>
2022-01-12 10:54:19 -0800

    adds all products and individual products routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+  def all_products
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def second_product
+    product = Product.find_by(id: 2)
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/all_products" => "products#all_products"
+  get "/first_product" => "products#first_product"
+  get "/second_product" => "products#second_product"
 end
