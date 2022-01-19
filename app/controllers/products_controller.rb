cff63eb1de0e90ab6b7c105ce3b841585c220bbc
dani <dani.zaghian@gmail.com>
2022-01-19 10:31:41 -0800

    adds happy and sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,8 +12,11 @@ class ProductsController < ApplicationController
       description: params[:description],
       quantity: params[:quantity]
     )
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -28,8 +31,11 @@ class ProductsController < ApplicationController
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
     product.quantity = params[:quantity] || product.quantity
-    product.save
-    render json: product
+    if product.save
+      render json: product
+    else
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   def destroy
