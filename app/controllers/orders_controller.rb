d91bcfb83d70c50c72ca1156bbf48b261f16168a
brian <riceb53@gmail.com>
2022-01-26 09:39:24 -0800

    add authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
   def index
-    orders = current_user.orders
-    render json: orders
+    if current_user
+      orders = current_user.orders
+      render json: orders
+    else
+      render json: [], status: :unauthorized
+    end
   end
 
   def create
